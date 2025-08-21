30fe7aa8747bd2c40df7c13621bd5c9251387227
Daniel <daniel.guo@vivalabs.com.au>
2025-08-21 13:32:47 +1000

    Move references projects to a separater folder

--- nodejs_core/event_loop/check_queue4.js
+++
@@ -1,2 +0,0 @@
-setTimeout(() => console.log("this is setTimeout 1"), 0);
-setImmediate(() => console.log("this is setImmediate 1"));
--- nodejs_core/event_loop/timer_queue3.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => console.log("this is setTimeout 1"), 1000);
-setTimeout(() => console.log("this is setTimeout 2"), 500);
-setTimeout(() => console.log("this is setTimeout 3"), 0);
